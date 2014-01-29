280acb65f3d67aa6c06265045c627345d8016802
Mith87 <jnaranjo_87@hotmail.com>
2014-01-29 14:57:24 -0600

    Agregado hibernate.cfg.xml, AppConfig.java class, cambio de nombre en carpeta view a views

---
+++ src/main/java/com/config/AppConfig.java
@@ -0,0 +1,80 @@
+package com.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@ComponentScan("com")
+@EnableWebMvc
+@EnableTransactionManagement
+@PropertySource("classpath:application.properties")
+
+public class AppConfig {
+	
+	private static final String DATABASE_DRIVER = "db.driver";
+    private static final String DATABASE_PASSWORD = "db.password";
+    private static final String DATABASE_URL = "db.url";
+    private static final String DATABASE_USERNAME = "db.username";
+    private static final String HIBERNATE_DIALECT = "hibernate.dialect";
+    private static final String HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+    private static final String ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
+	
+    private Environment e;
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(e.getRequiredProperty(DATABASE_DRIVER));
+        dataSource.setUrl(e.getRequiredProperty(DATABASE_URL));
+        dataSource.setUsername(e.getRequiredProperty(DATABASE_USERNAME));
+        dataSource.setPassword(e.getRequiredProperty(DATABASE_PASSWORD));
+        return dataSource;
+    }
+    
+	@Bean
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(getDataSource());
+        sessionFactoryBean.setPackagesToScan(e.getRequiredProperty(ENTITYMANAGER_PACKAGES_TO_SCAN));
+        sessionFactoryBean.setHibernateProperties(getHibernateProperties());
+        return sessionFactoryBean;
+    }
+
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.put(HIBERNATE_DIALECT, e.getRequiredProperty(HIBERNATE_DIALECT));
+        properties.put(HIBERNATE_SHOW_SQL, e.getRequiredProperty(HIBERNATE_SHOW_SQL));
+        return properties;
+    }
+
+    @Bean
+    public HibernateTransactionManager getTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(getSessionFactory().getObject());
+        return transactionManager;
+    }
+
+    @Bean
+    public UrlBasedViewResolver getViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+	
+}
